09d34333077be8b80fa709e88178e163d408326f
Supun D <supundunusingha2@gmail.com>
2024-04-09 22:11:23 +0530

    Update router.js

--- src/models/Doctor/router.js
+++ src/models/Doctor/router.js
@@ -9,6 +9,6 @@ const router = express.Router();
 router.route("/newdoctor").post(Validator.create, Controller.createDoctor);
 router.route("/get/alldoctors").get(Controller.getAllDoctors);
 router.route("/get/:id").get(Controller.getDoctorById);
-router.route("/update/:id").put(Validator.update, Controller.updatePharmacy);
+router.route("/update/:id").put(Validator.update, Controller.updateDoctor);
 router.route("/delete/:id").delete(Controller.deleteDoctor);
 module.exports = router;
