cb389fda31f270a06d7a1a30e4ccc920d068df01
Supun D <supundunusingha2@gmail.com>
2024-07-10 11:55:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require("./src");
 
 require("./cronJobs/presMedicineCron");
 require("./cronJobs/dailyMedicationCron");
-require("./cronJobs/medicineReminderCorn");
+// require("./cronJobs/medicineReminderCorn");
 
 const onListeningLog = `
    server is running on port : ${PORT} !!!`;
